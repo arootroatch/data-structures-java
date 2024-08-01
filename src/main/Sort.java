657b54ce0848226313689eff89a7d97c4f7d2a9d
Alex Root-Roatch <arootroatch@gmail.com>
2024-08-01 09:27:50 -0500

    fixed not returning empty

--- src/main/Sort.java
+++ src/main/Sort.java
@@ -5,31 +5,32 @@ public class Sort {
     boolean swapped;
     MyList<Integer> temp = list;
 
-    if (list instanceof MyLinkedList){
+    if (list.size() < 2) return list;
+
+    if (list instanceof MyLinkedList) {
       temp = new MyArrayList();
-      for (int i = 0; i < list.size(); i++){
+      for (int i = 0; i < list.size(); i++) {
         temp.add(list.get(i));
       }
     }
 
-    if (temp.size() > 1) {
-      do {
-        int start = 0;
-        swapped = false;
-        for (int i = start; i < temp.size() - 1; i++) {
-          if (temp.get(i) > temp.get(i + 1)) {
-            int elem = temp.get(i + 1);
-            temp.remove(i + 1);
-            temp.add(elem, i);
-            swapped = true;
-          }
+    do {
+      int start = 0;
+      swapped = false;
+      for (int i = start; i < temp.size() - 1; i++) {
+        if (temp.get(i) > temp.get(i + 1)) {
+          int elem = temp.get(i + 1);
+          temp.remove(i + 1);
+          temp.add(elem, i);
+          swapped = true;
         }
-      } while (swapped);
-    }
+      }
+    } while (swapped);
 
-    if (list instanceof  MyLinkedList){
+
+    if (list instanceof MyLinkedList) {
       MyLinkedList sorted = new MyLinkedList();
-      for (int i = 0; i < temp.size(); i++){
+      for (int i = 0; i < temp.size(); i++) {
         sorted.add(temp.get(i));
       }
       return sorted;
